462963eda1161fe1bc861aba496f5a0e5c4abad1
irene <irenepinillos.n@gmail.com>
2025-03-30 17:15:35 +0200

    modificación de favicon en index html, modificación de app component html e import en component ts

--- frontend/conecta/src/app/app.component.ts
+++ frontend/conecta/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router} from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FooterComponent,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
