7469a80f32fd893626bf58312373ee4f5239e51a
irene <irenepinillos.n@gmail.com>
2025-03-31 18:23:23 +0200

    app routing routes path; imagotipo en el header; index html background-color black

--- frontend/conecta/src/app/app.routes.ts
+++ frontend/conecta/src/app/app.routes.ts
@@ -1,4 +1,28 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { LayoutComponent } from './backoffice/layout/layout.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SidebarComponent } from './backoffice/sidebar/sidebar.component';
+import { HeaderBackofficeComponent } from './backoffice/header-backoffice/header-backoffice.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    //home
+    {path: "", component: HomeComponent},
+    {path: "sign-in", component: SignInComponent},
+    {path: "sign-up", component: SignUpComponent},
+    {path: "contact", component: ContactComponent},
+    
+
+    //backoffice
+    {
+        path: "backoffice", component: LayoutComponent, children: [
+            {path: "header-backoffice", component: HeaderBackofficeComponent},
+            {path: "sidebar", component: SidebarComponent},
+        ]
+    },
+
+];
 
