4eea37762c599ed6938ef334f225e52fffb5e6ed
navarro-n <irenepinillos.n@gmail.com>
2025-04-21 22:36:41 +0200

    cambio de estilo en las notas, cambio de diseño de scroll, cambio en la edición del titulo de la nota y el contenido. Cambio de selección en todo el proyecto, menos sign in, sign up y contact.

--- frontend/conecta/src/app/app.component.ts
+++ frontend/conecta/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet, Router} from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { CommonModule } from '@angular/common';
 import {HomeComponent} from './home/home.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
--- frontend/conecta/src/app/backoffice/note/note.component.ts
+++ frontend/conecta/src/app/backoffice/note/note.component.ts
@@ -1,21 +1,29 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { GridStack, GridStackOptions } from 'gridstack';
 import 'gridstack/dist/gridstack.min.css';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-note',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './note.component.html',
-  styleUrls: ['./note.component.scss'] 
+  styleUrls: ['./note.component.scss']
 })
 export class NoteComponent implements AfterViewInit {
   @ViewChild('gridStackElement') gridStackElement!: ElementRef;
   private gridStack!: GridStack;
   gridItems: any[] = [
-    { x: 1, y: 0, w: 2, h: 1, content: 'Nota 1' },
-    { x: 1, y: 0, w: 2, h: 1, content: 'Nota 2' },
-    { x: 1, y: 0, w: 2, h: 1, content: 'Nota 3' },
-    { x: 1, y: 0, w: 2, h: 1, content: 'Nota 4' }
+    { x: 1, y: 0, w: 2, h: 1, title: 'Nota Uno', content: 'Contenido de la nota 1' },
+    { x: 1, y: 1, w: 2, h: 1, title: 'Segunda Nota', content: 'Texto de la nota 2' },
+    { x: 3, y: 0, w: 2, h: 1, title: 'Tercera Idea', content: 'Detalles de la nota 3' },
+    { x: 3, y: 1, w: 2, h: 1, title: 'Cuarta Reflexión', content: 'El contenido de la nota 4' }
   ];
+
+  editingIndex: { [key: number]: boolean } = {};
 
   ngAfterViewInit(): void {
     const options: GridStackOptions = {};
@@ -25,5 +33,16 @@ export class NoteComponent implements AfterViewInit {
 
   loadGridItems(): void {
     this.gridStack.load(this.gridItems);
+  }
+
+  editNote(index: number): void {
+    this.editingIndex = {}; // Cerrar cualquier otra nota que esté en edición
+    this.editingIndex[index] = true;
+  }
+
+  saveNote(index: number): void {
+    this.editingIndex[index] = false;
+    // Aquí puedes añadir lógica para guardar los cambios en un servicio, base de datos, etc.
+    console.log('Nota guardada:', this.gridItems[index]);
   }
 }
