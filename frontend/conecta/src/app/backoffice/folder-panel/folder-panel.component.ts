bb1500dae35643a2610ae7a3e155cb2078450f0e
navarro-n <irenepinillos.n@gmail.com>
2025-05-17 22:33:49 +0200

    ajustes en la creación de nota y eliminado, falta dar estilo al popup de eliminar nota. También falta ajustar los margenes de header-panel y volver a poner la extensión de la pestaña del panel

--- frontend/conecta/src/app/backoffice/folder-panel/folder-panel.component.ts
+++ frontend/conecta/src/app/backoffice/folder-panel/folder-panel.component.ts
@@ -1,14 +1,19 @@
 import { Component, HostListener, Renderer2 } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-folder-panel',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgIf, FormsModule],
   templateUrl: './folder-panel.component.html',
   styleUrl: './folder-panel.component.scss'
 })
 export class FolderPanelComponent {
   private resizing = false;
+
+  notes: Array<{ name: string; editing: boolean }> = [];
 
   constructor(private renderer: Renderer2) {}
 
@@ -36,5 +41,52 @@ export class FolderPanelComponent {
       this.resizing = false;
       this.renderer.removeClass(document.body, 'resizing');
     }
+  }
+
+  addNote() {
+    this.notes.push({ name: '', editing: true });
+    setTimeout(() => {
+      const inputs = document.querySelectorAll('.note-input');
+      if (inputs.length) {
+        (inputs[inputs.length - 1] as HTMLInputElement).focus();
+      }
+    });
+  }
+
+  editNote(index: number) {
+    this.notes[index].editing = true;
+    setTimeout(() => {
+      const inputs = document.querySelectorAll('.note-input');
+      if (inputs[index]) {
+        (inputs[index] as HTMLInputElement).focus();
+      }
+    });
+  }
+
+  saveNote(index: number) {
+    this.notes[index].editing = false;
+    // Aquí podrías guardar en backend si lo necesitas
+  }
+
+  deleteNote(index: number) {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+      
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.notes.splice(index, 1);
+        Swal.fire({
+          title: 'Deleted!',
+          text: 'Your file has been deleted.',
+          icon: 'success'
+        });
+      }
+    });
   }
 }
