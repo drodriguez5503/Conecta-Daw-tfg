0ebcd7eb0166bbca48c72276ad4646faa20d0321
drodriguez5503 <davidrdelope@gmail.com>
2025-05-26 19:37:24 +0200

    GetUserInformation view hecho

--- backend/api_Conecta/api_users/urls.py
+++ backend/api_Conecta/api_users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from api_users.views import RegisterView, LoginView, CheckTokenView, GetUserByUsernameView, GetUserByIdView
+from api_users.views import RegisterView, LoginView, CheckTokenView, GetUserByUsernameView, GetUserByIdView, \
+    GetUserInformationView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -8,5 +9,6 @@ urlpatterns = [
     path('check-token/', CheckTokenView.as_view(), name='check-token'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('users/<str:username>', GetUserByUsernameView.as_view(), name='username'),
-    path('users/<int:pk>',GetUserByIdView.as_view(),name='id')
+    path('users/<int:pk>',GetUserByIdView.as_view(),name='id'),
+    path('users/info/', GetUserInformationView.as_view(), name='info')
 ]
